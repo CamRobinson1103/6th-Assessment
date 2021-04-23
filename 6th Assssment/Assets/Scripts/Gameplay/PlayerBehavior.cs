499fb562a6530764205c6f01795d084054392718
CamRobinson1103 <69812742+CamRobinson1103@users.noreply.github.com>
2021-04-22 19:01:23 -0500

    Adjusted the obstacles

--- 6th Assssment/Assets/Scripts/Gameplay/PlayerBehavior.cs
+++ 6th Assssment/Assets/Scripts/Gameplay/PlayerBehavior.cs
@@ -5,17 +5,7 @@ using UnityEngine;
 
 public class PlayerBehavior: MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 
     void OnCollisionEnter(Collision collision)
     {
